940e3426679e753d4b6541f0e10db4c2d7241530
Marco <spocram@gmail.com>
2022-03-31 11:20:58 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,6 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDPZ_gC2LIPqHd_IBvcx4SRTiP12Qb2VPI",
-    authDomain: "ombrellio.firebaseapp.com",
-    databaseURL: "https://ombrellio.firebaseio.com",
-    projectId: "ombrellio",
-    storageBucket: "ombrellio.appspot.com",
-    messagingSenderId: "747422293637",
-    appId: "1:747422293637:web:3299adb0ac90641d8853ec",
-    measurementId: "G-L1R1J8Y25R"
+
   }
 };
